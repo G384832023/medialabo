4796ec476dbc45317ac9ada1ced84fcd81b8f3ba
G384832023 <238483@st.takushoku-u.ac.jp>
2024-05-23 12:32:44 +0900

    kazuate.jsフォルダにファイルを設置

---
+++ kazuate.js
@@ -0,0 +1,37 @@
+// 答え
+let kotae = Math.floor(Math.random()*10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
+
+// 入力回数（予想回数）
+let kaisu = 7;
+
+// 予想を4回実行する
+// 将来以下の hantei(); の4回の呼び出しを全て削除する
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+hantei();
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso = 6;
+  for(let i=1;i<=kaisu;i++){
+    if(i>=4){
+      console.log(i + "回目の予想:" + yoso);
+      console.log("答えは" + kotae + "でした。すでにゲームは終わってます。");
+    }else{
+      if(yoso === kotae){
+        console.log(i + "回目の予想:" + yoso);
+        console.log("正解です。おめでとう!");
+      }else if(yoso < kotae){
+        console.log(i + "回目の予想:" + yoso);
+        console.log("まちがい。答えはもっと大きいですよ。");
+      }else{
+        console.log(i + "回目の予想:" + yoso);
+        console.log("まちがい。答えはもっと小さいですよ。");
+      }
+    }
+  }
+}
+  // 課題3-1: 正解判定する
+  // kotae と yoso が一致するかどうか調べて結果を出力
+  // 課題3-1における出力先はコンソール
