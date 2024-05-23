7723d6b0911df71e3c1e9e25250ce675c9693b88
G384832023 <238483@st.takushoku-u.ac.jp>
2024-05-23 17:34:27 +0900

    practice-domフォルダにファイルを設置

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -15,12 +15,46 @@ let data = [
 
 // 練習4-2 メッセージ追加プログラム
 
+let h2 =document.querySelector('h2#ex42');
+let p = document.createElement('p');
+p.textContent = '写真表と都市の緯度軽度のページです';
+h2.insertAdjacentElement('afterend',p); 
+p.style.textEmphasis = "sesame green"; 
 
 // 練習4-3 写真表作成プログラム
 
+let div1 = document.querySelector('div#phototable');
+
+let Link1 = document.createElement('img');
+Link1.setAttribute('src','taro.png'); 
+let p1 = document.createElement('p');
+p1.insertAdjacentElement('beforeend',Link1);
+div1.insertAdjacentElement('beforeend',p1); 
+
+let Link2 = document.createElement('img');
+Link2.setAttribute('src','jiro.png'); 
+let p2 = document.createElement('p');
+p2.insertAdjacentElement('beforeend',Link2);
+div1.insertAdjacentElement('beforeend',p2); 
+
+let Link3 = document.createElement('img');
+Link3.setAttribute('src','hanako.png'); 
+let p3 = document.createElement('p');
+p3.insertAdjacentElement('beforeend',Link3);
+div1.insertAdjacentElement('beforeend',p3); 
 
 // 練習4-4 箇条書き削除プログラム
 
+let w = document.querySelectorAll('li');
+for(let w1 of w){
+   w1.remove();
+} 
 
 // 練習4-5 箇条書き追加プログラム
 
+let ul = document.querySelector('ul#location');
+for(let d of data){
+	let li = document.createElement('li');
+	li.textContent = d.name + " ... 緯度:" + d.lat +", 経度:" + d.lng;
+	ul.insertAdjacentElement('beforeend',li); 
+ }
