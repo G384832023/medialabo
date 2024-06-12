c72d30b83bb218da7ecea710adfd34dfa1216f98
G384832023 <238483@st.takushoku-u.ac.jp>
2024-06-12 16:47:19 +0900

    weather.jsフォルダにファイルを設置

--- weather.js
+++ weather.js
@@ -47,7 +47,7 @@ let data = {
 
 ////////// 課題3-2 ここからプログラムを書こう
 
-console.log(data.coord.lon);
+/*console.log(data.coord.lon);
 console.log(data.coord.lat);
 console.log(data.weather[0].description);
 console.log(data.main.temp_min);
@@ -56,39 +56,71 @@ console.log(data.main.humidity);
 console.log(data.wind.speed);
 console.log(data.wind.deg);
 console.log(data.name);
+*/
 
 let div = document.querySelector('div#result');
-
 let h1 = document.createElement('h1');
 h1.textContent = "世界の天気";
 div.insertAdjacentElement('beforeend',h1); 
 let h2 = document.createElement('h2');
-h2.textContent = "検索結果1件";
-h1.insertAdjacentElement('afterend',h2); 
 let li1 = document.createElement('li');
-li1.textContent = "緯度: " + data.coord.lon;
-h2.insertAdjacentElement('afterend',li1); 
 let li2 = document.createElement('li');
-li2.textContent = "経度: " + data.coord.lat;
-li1.insertAdjacentElement('afterend',li2); 
 let li3 = document.createElement('li');
-li3.textContent = "天気: " + data.weather[0].description;
-li2.insertAdjacentElement('afterend',li3); 
 let li4 = document.createElement('li');
-li4.textContent = "最低気温: " + data.main.temp_min;
-li3.insertAdjacentElement('afterend',li4); 
 let li5 = document.createElement('li');
-li5.textContent = "最高気温: " + data.main.temp_max;
-li4.insertAdjacentElement('afterend',li5); 
 let li6 = document.createElement('li');
-li6.textContent = "湿度: " + data.main.humidity;
-li5.insertAdjacentElement('afterend',li6); 
 let li7 = document.createElement('li');
-li7.textContent = "風速: " + data.wind.speed;
-li6.insertAdjacentElement('afterend',li7); 
 let li8 = document.createElement('li');
-li8.textContent = "風向: " + data.wind.deg;
-li7.insertAdjacentElement('afterend',li8); 
 let li9 = document.createElement('li');
-li9.textContent = "都市名: " + data.name;
-li8.insertAdjacentElement('afterend',li9); 
+
+let b = document.querySelector('#print');
+b.addEventListener('click', sendRequest);
+function sendRequest() {
+  let s = document.querySelector('select#sentaku');
+  let idx = s.selectedIndex;  // idx 番目の option が選択された
+  let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+  let o = os.item(idx);       // os の idx 番目の要素
+  let v = o.getAttribute('value');
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + v + '.json';
+
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+}
+
+function showResult(resp) {
+	let data = resp.data;
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+  h2.textContent = "検索結果1件";
+  div.insertAdjacentElement('beforeend',h2); 
+  li1.textContent = "緯度: " + data.coord.lon;
+  div.insertAdjacentElement('beforeend',li1); 
+  li2.textContent = "経度: " + data.coord.lat;
+  div.insertAdjacentElement('beforeend',li2); 
+  li3.textContent = "天気: " + data.weather[0].description;
+  div.insertAdjacentElement('beforeend',li3); 
+  li4.textContent = "最低気温: " + data.main.temp_min;
+  div.insertAdjacentElement('beforeend',li4); 
+  li5.textContent = "最高気温: " + data.main.temp_max;
+  div.insertAdjacentElement('beforeend',li5); 
+  li6.textContent = "湿度: " + data.main.humidity;
+  div.insertAdjacentElement('beforeend',li6); 
+  li7.textContent = "風速: " + data.wind.speed;
+  div.insertAdjacentElement('beforeend',li7);
+  li8.textContent = "風向: " + data.wind.deg;
+  div.insertAdjacentElement('beforeend',li8); 
+  li9.textContent = "都市名: " + data.name;
+  div.insertAdjacentElement('beforeend',li9); 
+}
+
+function showError(err) {
+	console.log(err);
+}	
+
+function finish() {
+	console.log('Ajax 通信が終わりました');
+}
