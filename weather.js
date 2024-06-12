52e2c42fa3ccd306822f865c9032001bec118f26
G384832023 <238483@st.takushoku-u.ac.jp>
2024-06-12 23:28:01 +0900

    weather.jsフォルダにファイルを設置

--- weather.js
+++ weather.js
@@ -61,6 +61,8 @@ console.log(data.name);
 let div = document.querySelector('div#result');
 let h1 = document.createElement('h1');
 h1.textContent = "世界の天気";
+h1.style.borderBottom = 'double 5px #FFC778';
+h1.style.fontSize='60px';
 div.insertAdjacentElement('beforeend',h1); 
 let h2 = document.createElement('h2');
 let li1 = document.createElement('li');
@@ -96,24 +98,43 @@ function showResult(resp) {
 	}
 
   h2.textContent = "検索結果1件";
+  h2.style.fontSize='30px';
   div.insertAdjacentElement('beforeend',h2); 
   li1.textContent = "緯度: " + data.coord.lon;
+  li1.style.color = 'yellow';
+  li1.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li1); 
   li2.textContent = "経度: " + data.coord.lat;
+  li2.style.color = 'yellow';
+  li2.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li2); 
   li3.textContent = "天気: " + data.weather[0].description;
+  li3.style.color = 'yellow';
+  li3.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li3); 
   li4.textContent = "最低気温: " + data.main.temp_min;
+  li4.style.color = 'yellow';
+  li4.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li4); 
   li5.textContent = "最高気温: " + data.main.temp_max;
+  li5.style.color = 'yellow';
+  li5.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li5); 
   li6.textContent = "湿度: " + data.main.humidity;
+  li6.style.color = 'yellow';
+  li6.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li6); 
   li7.textContent = "風速: " + data.wind.speed;
+  li7.style.color = 'yellow';
+  li7.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li7);
   li8.textContent = "風向: " + data.wind.deg;
+  li8.style.color = 'yellow';
+  li8.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li8); 
   li9.textContent = "都市名: " + data.name;
+  li9.style.color = 'yellow';
+  li9.style.fontSize='20px';
   div.insertAdjacentElement('beforeend',li9); 
 }
 
